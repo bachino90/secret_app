462b6653a1806be2470c3915cbfdd98d9d8ef666
bachino90 <emilianobivachi@gmail.com>
2014-03-02 13:42:01 -0300

    Change in secret model

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,3 +1,43 @@
+class SecretModel
+  attr_accessor :id
+  attr_accessor :content
+  attr_accessor :media_url
+  attr_accessor :about
+  attr_accessor :created_at
+
+  attr_accessor :comments_count
+  attr_accessor :friends_comments_count
+  attr_accessor :likes_count
+  attr_accessor :friends_likes_count
+
+  attr_accessor :author_is_friend
+  attr_accessor :i_am_author
+  attr_accessor :i_like_it
+
+  def initialize(secret, user)
+    @id = secret.id
+    @content = secret.content
+    @media_url = secret.media_url
+    @about = secret.about
+    @created_at = secret.created_at
+    @comments_count = secret.comments.count
+    @likes_count = secret.likes_count
+    @author_is_friend = false
+    @i_am_author = false
+    @i_like_it = false
+    if user.friends.include?(secret.user_id.to_s)
+        @author_is_friend = true
+      elsif secret.user_id.to_s == user.id.to_s
+        @i_am_author = true
+      end
+      if secret.likes.include?(user.id.to_s)
+        @i_like_it = true
+      end
+  end
+end
+
+
+
 class Secret
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -7,38 +47,41 @@ class Secret
   field :about, type: String, default: "00"
   field :likes, type: Array, default: []
 
-  field :comments_count, type: Integer, default: 0
-  field :friends_comments_count, type: Integer, default: 0
+  #field :comments_count, type: Integer, default: 0
+  #field :friends_comments_count, type: Integer, default: 0
   field :likes_count, type: Integer, default: 0
-  field :friends_likes_count, type: Integer, default: 0
+  #field :friends_likes_count, type: Integer, default: 0
 
-  field :author_is_friend, type: Boolean, default: false
-  field :i_am_author, type: Boolean, default: false
-  field :i_like_it, type:Boolean, default: false
+  #field :author_is_friend, type: Boolean, default: false
+  #field :i_am_author, type: Boolean, default: false
+  #field :i_like_it, type:Boolean, default: false
 
 	belongs_to :user, inverse_of: :secrets #index: true
   embeds_many :comments
 
+  #index ({about: 1}, {name: "about_index"})
+
   def self.secretsIndex(user,about)
-    if about && about != '00'
+    if about && about != '00' && about != ''
       secrets = Secret.all_in(about: about).desc(:created_at)
     else
       secrets = Secret.all.desc(:created_at)
     end
-    secrets.each do |s|
-      s.comments_count = s.comments.count
-      s.likes_count = s.likes.count
-      if user.friends.include?(s.user_id.to_s)
-        s.author_is_friend = true
-      elsif s.user_id.to_s == user.id.to_s
-        s.i_am_author = true
-      end
-      if s.likes.include?(user.id.to_s)
-        s.i_like_it = true
-      end
+    secretsModel = Array.new(secrets.count)
+    secrets.each_with_index do |s, i|
+      #s.comments_count = s.comments.count
+      #if user.friends.include?(s.user_id.to_s)
+      #  s.author_is_friend = true
+      #elsif s.user_id.to_s == user.id.to_s
+      #  s.i_am_author = true
+      #end
+      #if s.likes.include?(user.id.to_s)
+      #  s.i_like_it = true
+      #end
+      secretsModel[i] = SecretModel.new(s, user)
     end
-    return secrets
+
+    return secretsModel
   end
 
-  #index ({about: 1}, {name: "about_index"})
 end
--- app/views/api/v1/secrets/index.json.jbuilder
+++ app/views/api/v1/secrets/index.json.jbuilder
@@ -1,3 +1,3 @@
 json.array!(@secrets) do |secret|
-  json.extract! secret, :id, :content, :media_url, :about, :comments_count, :likes_count, :likes, :i_am_author, :author_is_friend, :i_like_it, :created_at, :comments
+  json.extract! secret, :id, :content, :media_url, :about, :comments_count, :likes_count, :i_am_author, :author_is_friend, :i_like_it, :created_at
 end
